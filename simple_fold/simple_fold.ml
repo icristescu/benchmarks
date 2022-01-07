1af3e8cc0923ac736ff2d2c1b310ae3fc62fc1c1
icristescu <domninac@gmail.com>
2022-01-07 11:53:29 +0100

    simplify example

--- simple_fold/simple_fold.ml
+++ simple_fold/simple_fold.ml
@@ -56,24 +56,8 @@ type ('v, 'acc, 'r) folder =
 
 type 'a node_fn = step list -> step list -> 'a -> 'a Lwt.t
 
-let fold :
-    type acc.
-    order:[ `Sorted | `Undefined | `Random ] ->
-    force:bool ->
-    cache:bool ->
-    uniq:bool ->
-    pre:acc node_fn option ->
-    post:acc node_fn option ->
-    path:step list ->
-    ?depth:int ->
-    node:(step list -> _ -> acc -> acc Lwt.t) ->
-    contents:(step list -> string -> acc -> acc Lwt.t) ->
-    tree:(step list -> _ -> acc -> acc Lwt.t) ->
-    t ->
-    acc ->
-    acc Lwt.t =
- fun ~order:_ ~force:_ ~cache:_ ~uniq:_ ~pre:_ ~post:_ ~path ?depth:_ ~node:_
-     ~contents:_ ~tree:_ t acc ->
+let fold : type acc. path:step list -> t -> acc -> acc Lwt.t =
+ fun ~path t acc ->
   let counter = ref 0 in
   let rec aux : type r. (t, acc, r) folder =
    fun ~path acc d t k ->
@@ -117,53 +101,7 @@ let fold :
   in
   aux_uniq ~path acc 0 t Lwt.return
 
-(* fun ~order:_ ~force:_ ~cache:_ ~uniq:_ ~pre:_ ~post:_ ~path ?depth:_ ~node:_
- *     ~contents:_ ~tree:_ t acc ->
- *  let counter = ref 0 in
- *  let rec aux : type r. (t, acc, r) folder =
- *   fun ~path acc t k ->
- *    let next acc =
- *      match cached_map t with
- *      | Some n -> (map [@tailcall]) ~path acc (Some n) k
- *      | None -> k acc
- *    in
- *    next acc
- *  and aux_uniq : type r. (t, acc, r) folder =
- *   fun ~path acc t k -> (aux [@tailcall]) ~path acc t k
- *  and step : type r. (step * elt, acc, r) folder =
- *   fun ~path acc (s, _v) k ->
- *    let _path = rcons path s in
- *    let apply () =
- *      incr counter;
- *      if !counter mod 20_000 = 0 then stack_size !counter;
- *      k acc
- *    in
- *    apply ()
- *  and steps : type r. ((step * elt) Seq.t, acc, r) folder =
- *   fun ~path acc s k ->
- *    match s () with
- *    | Seq.Nil -> (k [@tailcall]) acc
- *    | Seq.Cons (h, t) ->
- *        (step [@tailcall]) ~path acc h (fun acc ->
- *            (steps [@tailcall]) ~path acc t k)
- *  and map : type r. (map option, acc, r) folder =
- *   fun ~path acc m k ->
- *    match m with
- *    | None -> k acc
- *    | Some m ->
- *        let bindings = StepMap.to_seq m in
- *        seq ~path acc bindings k
- *  and seq : type r. ((step * elt) Seq.t, acc, r) folder =
- *   fun ~path acc bindings k -> (steps [@tailcall]) ~path acc bindings k
- *  in
- *  aux_uniq ~path acc t Lwt.return *)
-
 let id _ _ acc = Lwt.return acc
-
-let fold ?(order = `Sorted) ?(force = true) ?(cache = false) ?(uniq = false)
-    ?pre ?post ?depth ?(contents = id) ?(node = id) ?(tree = id) t acc =
-  fold ~order ~force ~cache ~uniq ~pre ~post ~path:[] ?depth ~contents ~node
-    ~tree t acc
 
 let test () =
   let size = 830829 in
@@ -171,6 +109,6 @@ let test () =
     List.init size string_of_int
     |> List.fold_left (fun acc i -> add acc i (`Contents i)) empty
   in
-  fold t [] >|= ignore
+  fold ~path:[] t [] >|= ignore
 
 let () = Lwt_main.run (test ())
