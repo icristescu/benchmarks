acaef1ff8118b12b4c3440774d47de3581106896
icristescu <domninac@gmail.com>
2022-01-07 11:13:23 +0100

    add an example

---
+++ simple_fold/simple_fold.ml
@@ -0,0 +1,68 @@
+module StepMap = struct
+  module X = struct
+    type t = string
+
+    let compare = String.compare
+  end
+
+  include Map.Make (X)
+end
+
+type map = string StepMap.t
+
+and t = Map of map
+
+let of_map m = Map m
+let empty = of_map StepMap.empty
+
+let add t step v =
+  let (Map m) = t in
+  StepMap.add step v m |> of_map
+
+let rcons t s = t @ [ s ]
+
+let stack_size p =
+  let stats = Gc.stat () in
+  Fmt.epr "stack_size at position %d is %d \n%!" p stats.Gc.stack_size
+
+type ('a, 'r) cont = ('a -> 'r) -> 'r
+
+type ('v, 'acc, 'r) folder =
+  path:string list -> 'acc -> int -> 'v -> ('acc, 'r) cont
+
+(* If we remove the path or the depth argument the test passes. *)
+let fold : type acc. path:string list -> t -> acc -> acc =
+ fun ~path t acc ->
+  let counter = ref 0 in
+  let rec step : type r. (string * string, acc, r) folder =
+   fun ~path:_ acc _d (_s, _v) k ->
+    let apply () =
+      incr counter;
+      if !counter mod 20_000 = 0 then stack_size !counter;
+      k acc
+    in
+    apply ()
+  and steps : type r. ((string * string) Seq.t, acc, r) folder =
+   fun ~path acc d s k ->
+    match s () with
+    | Seq.Nil -> (k [@tailcall]) acc
+    | Seq.Cons (h, t) ->
+        (step [@tailcall]) ~path acc d h (fun acc ->
+            (steps [@tailcall]) ~path acc d t k)
+  and map : type r. (t, acc, r) folder =
+   fun ~path acc d t k ->
+    let (Map m) = t in
+    let bindings = StepMap.to_seq m in
+    (steps [@tailcall]) ~path acc d bindings k
+  in
+  map ~path acc 0 t Fun.id
+
+let test () =
+  let size = 830829 in
+  let t =
+    List.init size string_of_int
+    |> List.fold_left (fun acc i -> add acc i i) empty
+  in
+  fold ~path:[] t [] |> ignore
+
+let () = test ()
