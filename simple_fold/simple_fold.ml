1e07f6bcfbbf92a9599164df3d637d5abdabbe7b
icristescu <domninac@gmail.com>
2022-01-07 12:03:37 +0100

    modify example

--- simple_fold/simple_fold.ml
+++ simple_fold/simple_fold.ml
@@ -21,20 +21,28 @@ and info = { mutable map : map option }
 
 and v = Map of map
 
-and t = { mutable v : v; info : info }
+and t = { v : v }
+
+(* let of_map m =
+ *   let info = { map = Some m } in
+ *   let v = Map m in
+ *   { v; info }
+ * 
+ * let cached_map t =
+ *   match (t.v, t.info.map) with
+ *   | Map m, None ->
+ *       let m = Some m in
+ *       t.info.map <- m;
+ *       m
+ *   | _, m -> m *)
 
 let of_map m =
-  let info = { map = Some m } in
   let v = Map m in
-  { v; info }
+  { v }
 
-let cached_map t =
-  match (t.v, t.info.map) with
-  | Map m, None ->
-      let m = Some m in
-      t.info.map <- m;
-      m
-  | _, m -> m
+let cached_map { v } =
+  let (Map m) = v in
+  Some m
 
 let empty = of_map StepMap.empty
 
@@ -48,26 +56,28 @@ let stack_size p =
   let stats = Gc.stat () in
   Fmt.epr "stack_size at position %d is %d \n%!" p stats.Gc.stack_size
 
-type ('a, 'r) cont = ('a -> 'r) -> 'r
-type ('a, 'r) cont_lwt = ('a, 'r Lwt.t) cont
-type ('v, 'acc, 'r) folder = path:step list -> 'acc -> 'v -> ('acc, 'r) cont_lwt
+type ('a, 'r) cont = ('a -> 'r Lwt.t) -> 'r Lwt.t
+
+type ('v, 'acc, 'r) folder =
+  path:step list -> 'acc -> int -> 'v -> ('acc, 'r) cont
+
 type 'a node_fn = step list -> step list -> 'a -> 'a Lwt.t
 
 let fold : type acc. path:step list -> t -> acc -> acc Lwt.t =
  fun ~path t acc ->
   let counter = ref 0 in
   let rec aux : type r. (t, acc, r) folder =
-   fun ~path acc t k ->
+   fun ~path acc d t k ->
     let next acc =
       match cached_map t with
-      | Some n -> (map [@tailcall]) ~path acc (Some n) k
+      | Some n -> (map [@tailcall]) ~path acc d (Some n) k
       | None -> k acc
     in
     next acc
   and aux_uniq : type r. (t, acc, r) folder =
-   fun ~path acc t k -> (aux [@tailcall]) ~path acc t k
+   fun ~path acc d t k -> (aux [@tailcall]) ~path acc d t k
   and step : type r. (step * elt, acc, r) folder =
-   fun ~path acc (s, _v) k ->
+   fun ~path acc _d (s, _v) k ->
     let _path = rcons path s in
     let apply () =
       incr counter;
@@ -76,23 +86,23 @@ let fold : type acc. path:step list -> t -> acc -> acc Lwt.t =
     in
     apply ()
   and steps : type r. ((step * elt) Seq.t, acc, r) folder =
-   fun ~path acc s k ->
+   fun ~path acc d s k ->
     match s () with
     | Seq.Nil -> (k [@tailcall]) acc
     | Seq.Cons (h, t) ->
-        (step [@tailcall]) ~path acc h (fun acc ->
-            (steps [@tailcall]) ~path acc t k)
+        (step [@tailcall]) ~path acc d h (fun acc ->
+            (steps [@tailcall]) ~path acc d t k)
   and map : type r. (map option, acc, r) folder =
-   fun ~path acc m k ->
+   fun ~path acc d m k ->
     match m with
     | None -> k acc
     | Some m ->
         let bindings = StepMap.to_seq m in
-        seq ~path acc bindings k
+        seq ~path acc d bindings k
   and seq : type r. ((step * elt) Seq.t, acc, r) folder =
-   fun ~path acc bindings k -> (steps [@tailcall]) ~path acc bindings k
+   fun ~path acc d bindings k -> (steps [@tailcall]) ~path acc d bindings k
   in
-  aux_uniq ~path acc t Lwt.return
+  aux_uniq ~path acc 0 t Lwt.return
 
 let id _ _ acc = Lwt.return acc
 
